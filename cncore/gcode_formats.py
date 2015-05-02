38e930b912a6cb041d98fbec629f3eb468afb728
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2015-05-02 15:09:32 -0700

    Add another gcode format

--- cncore/gcode_formats.py
+++ cncore/gcode_formats.py
@@ -3,3 +3,8 @@ PROTOTRAK = dict(number_lines = True,
                   line_ending = '\r\n',
                   space = False,
                   program_number = None)
+
+GRBL = dict(number_lines = False,
+            line_endings = '\n',
+            space = True,
+            program_number = None)
